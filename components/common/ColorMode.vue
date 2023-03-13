e23b5c24c80a4ad69d85ebbfb2d4545548ebaa5c
ZSoulaimane <zar.souler@gmail.com>
2023-03-13 21:29:30 +0000

    REFACTOR - index

--- components/common/ColorMode.vue
+++ components/common/ColorMode.vue
@@ -1,13 +1,5 @@
 <template>
     <button class="edublink-vue-light-dark-switcher" @click="toggleTheme()">
-        <ColorScheme placeholder=''>
-            <span v-if="$colorMode.value === 'dark'">
-                <inline-svg src="/images/light-dark/sun.svg" alt="light icon" />
-            </span>
-            <span v-else>
-                <inline-svg src="/images/light-dark/moon.svg" alt="dark icon" />
-            </span>
-        </ColorScheme>
     </button>
 </template>
 
@@ -15,7 +7,7 @@
 export default {
     methods: {
         toggleTheme() {
-            this.$colorMode.preference = this.$colorMode.preference !== 'dark' ? 'dark' : 'dark';
+            this.$colorMode.preference = 'dark';
         }
     }
 }
